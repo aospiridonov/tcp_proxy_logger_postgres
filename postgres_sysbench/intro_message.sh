7e01eac74262e159bd83200607a8de827815c640
aospiridonov <aospiridonov@gmail.com>
2022-11-22 15:50:05 +0300

    add postgress sysbench docker container

---
+++ postgres_sysbench/intro_message.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo -e "\n\n-----\n./run_sysbench.sh to start sysbench on postgres database\n-----\n"
+
+exec "$@"
---
+++ postgres_sysbench/run_sysbench.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+for t in $(seq 1 100) 
+do
+  PGPASSWORD=${DB_PASSWORD:-root} psql --host=${DB_HOST:-localhost} \
+       --port=${DB_PORT:-5432}\
+       --username=${DB_USERNAME:-admin} \
+       --dbname=${DB_NAME:-postgres} \
+       --command="DROP TABLE IF EXISTS sbtest${t}" > /dev/null 2>&1
+done
+
+sysbench --pgsql-host=${DB_HOST:-localhost} \
+         --pgsql-port=${DB_PORT:-5432} \
+         --pgsql-user=${DB_USERNAME:-admin} \
+         --pgsql-password=${DB_PASSWORD:-root} \
+         --pgsql-db=${DB_NAME:-postgres} \
+         /usr/local/share/sysbench/oltp_read_write.lua prepare
+
+sysbench --pgsql-host=${DB_HOST:-localhost} \
+         --pgsql-port=${DB_PORT:-5432} \
+         --pgsql-user=${DB_USERNAME:-admin} \
+         --pgsql-password=${DB_PASSWORD:-password} \
+         --pgsql-db=${DB_NAME:-postgres} \
+         --threads=${SYSBENCH_THREADS:-1} \
+         --time=${SYSBENCH_TIME:-60} \
+         /usr/local/share/sysbench/oltp_read_write.lua run
